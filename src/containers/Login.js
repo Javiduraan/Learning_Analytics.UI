78b3f6d2f694f29567c527d863c5b162bf780911
Javier Duran <Javier.Duran1@hotmail.com>
2021-10-03 22:08:06 -0600

    Initial Commit

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import  Button from "react-bootstrap/Button";
+import axios from "axios";
 import "./Login.css";
 
 export default function Login() {
@@ -8,7 +9,26 @@ export default function Login() {
     const [password, setPassword] = useState("");
 
     function validateForm() {
-        return email.length > 0 && password.length > 0;
+        //tomar el username = email y el pwd 
+        //hacer un get.
+        //si nos devuelve un json con los datos del usuario
+        //entonces es un usuario valido
+        //si no, mostrar mensaje de error.
+        axios.post('https://localhost:5001/api/User/Auth', {
+            username: email,
+            password: password
+        })
+        .then((res) => {
+            if (res.status === 200) {
+                console.log('Usuario valido');
+                return true;
+            }
+        })
+        .catch((err) => {
+            console.log('usuario no valido');
+            console.log(err);
+        })
+        // return email.length > 0 && password.length > 0;
     }
 
     function handleSubmit(event) {
@@ -19,10 +39,10 @@ export default function Login() {
         <div className="Login">
             <Form onSubmit={handleSubmit}>
                 <Form.Group size="lg" controlId="email">
-                    <Form.Label>Email</Form.Label>
+                    <Form.Label>Username</Form.Label>
                     <Form.Control
                       autoFocus
-                      type="email"
+                      type="text"
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       />
@@ -36,7 +56,7 @@ export default function Login() {
                       onChange={(e) => setPassword(e.target.value)}
                       />
                 </Form.Group>
-                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                <Button block size="lg" type="submit" onClick={validateForm}>
                   Login
                 </Button>
             </Form>
